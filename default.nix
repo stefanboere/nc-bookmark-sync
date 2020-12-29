f765de4ab2a9a9c44f85a96ffb053b9dd0183bf9
Stefan Boere <s.boere@hotmail.com>
2020-12-30 00:18:10 +0100

    Build with nix

--- default.nix
+++ default.nix
@@ -1,7 +1,22 @@
 let
   pkgs = import ./nix { };
 
-  packages = { };
+  nc-bookmark-sync = pkgs.rustPlatform.buildRustPackage {
+    pname = "nc-bookmark-sync";
+    version = "0.0.1";
+
+    buildInputs = with pkgs; [ openssl ];
+    nativeBuildInputs = with pkgs; [ pkgconfig ];
+
+    src = pkgs.gitignoreSource ./.;
+
+    cargoSha256 = "1x102fwbh2jy0ywn9ab9mr1b5j1ly91k2j7avz1ipx15chznxb5c";
+
+    meta = with pkgs.stdenv.lib; {
+      description = "Sync Nextcloud bookmarks with a plain text file.";
+      license = licenses.mit;
+    };
+  };
 
   # Development shell
   devshell = pkgs.stdenv.mkDerivation {
@@ -18,7 +33,4 @@ let
     inherit (pkgs.pre-commit-check) shellHook;
   };
 
-in {
-  inherit devshell;
-
-} // packages
+in { inherit devshell nc-bookmark-sync; }
