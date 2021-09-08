a09dfb919864fdd24bf73d9d6ac551d3c537c3b4
Stefan Boere <s.boere@hotmail.com>
2021-09-08 22:24:42 +0200

    Use flakes

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Sync Nextcloud bookmarks with the local file system";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+
+    naersk.url = "github:nix-community/naersk";
+
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, naersk, pre-commit-hooks, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        naersk-lib = naersk.lib.${system};
+      in rec {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixfmt.enable = true;
+              rustfmt.enable = true;
+            };
+          };
+        };
+
+        # `nix build`
+        packages.nc-bookmark-sync = naersk-lib.buildPackage {
+          pname = "nc-bookmark-sync";
+          version = "0.0.1";
+
+          buildInputs = with pkgs; [ openssl ];
+          nativeBuildInputs = with pkgs; [ pkgconfig ];
+
+          root = ./.;
+
+          meta = with pkgs.lib; {
+            description = "Sync Nextcloud bookmarks with a plain text file.";
+            license = licenses.mit;
+          };
+        };
+        defaultPackage = packages.nc-bookmark-sync;
+
+        # `nix run`
+        apps.nc-bookmark-sync =
+          flake-utils.lib.mkApp { drv = packages.nc-bookmark-sync; };
+        defaultApp = apps.nc-bookmark-sync;
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+            rls
+            rustfmt
+            openssl
+            pkgconfig
+          ];
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        };
+      });
+}
